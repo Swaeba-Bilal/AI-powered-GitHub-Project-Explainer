4149d0687a9ec6ab0b7f647b326712c61c701d21
swaiba-bilal <swaibabilal02@gmail.com>
2026-02-07 18:52:43 +0500

    initial setup

---
+++ src/main/java/com/example/projectexplainer/ProjectexplainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectexplainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectexplainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectexplainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectexplainer/ProjectexplainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectexplainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectexplainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
